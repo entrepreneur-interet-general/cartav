0a5675bd33a2aff64bf373e8f640187880c62dd6
Tristram Gräbener <tristram.grabener@interieur.gouv.fr>
2017-07-12 11:31:34 +0200

    Nouvelles bases de prod

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,10 +4,10 @@ var prodEnv = require('./prod.env')
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   indices: {
-    acc: 'es5dev_2005_2015_accidents',
-    pve: 'es5dev_2014_2015_pve',
-    acc_usagers: 'es5dev_accidents_usagers',
-    acc_vehicules: 'es5dev_accidents_vehicules',
-    radars: 'es5dev_radars'
+    acc: '"es5dev_2005_2015_accidents"',
+    pve: '"es5dev_2014_2015_pve"',
+    acc_usagers: '"es5dev_accidents_usagers"',
+    acc_vehicules: '"es5dev_accidents_vehicules"',
+    radars: '"es5dev_radars"'
   }
 })
